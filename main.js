fb864e2765558fc79f09f5785cc649b40a560b89
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-04-04 01:03:01 -0300

    Portfolio em desenvolvimento

---
+++ main.js
@@ -0,0 +1,15 @@
+let typing = new Typed(".animate", {
+  strings: [
+    "",
+    "Front-End Developer",
+    "",
+    "Curious",
+    "",
+    "UX/UI Lover",
+    "",
+    "Passionate about web development",
+  ],
+  typeSpeed: 120,
+  backSpeed: 70,
+  loop: true,
+});
