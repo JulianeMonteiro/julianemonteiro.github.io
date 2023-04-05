fd6ea363d4e71bab65b8ba674974ecd726c834c8
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-04-05 09:50:13 -0300

    Projeto em fasee final

--- main.js
+++ main.js
@@ -13,3 +13,9 @@ let typing = new Typed(".animate", {
   backSpeed: 70,
   loop: true,
 });
+
+let btn = document.querySelector("#back-to-top");
+
+btn.addEventListener("click", function () {
+  window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
+});
