29357d3aed0f743888fcac65fe1090195fae360c
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-04-10 18:41:56 -0300

    Code review em andamento

--- main.js
+++ main.js
@@ -19,24 +19,3 @@ let btn = document.querySelector("#back-to-top");
 btn.addEventListener("click", function () {
   window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
 });
-
-document.querySelector(".button").addEventListener("click", () => {
-  document.querySelector(".sidebar").classList.toggle("isOpen");
-});
-
-window.onclick = (e) => {
-  // seleciona o .menu__list
-  let menu = document.querySelector(".menu__list");
-
-  // verifica se está fechado ou aberto
-  let open = menu.classList.contains("isOpen");
-
-  // verifica se o elemento clicado contém a classe .menu__list
-  let alv = e.target.classList.contains("menu__list");
-
-  // verifica se o elemento clicado é filho de .menu__list
-  let father = e.target.closest(".menu__list");
-
-  // se tudo for falso, fecha o menu__list
-  if (!open && !alv && !father) menu.classList.toggle("isOpen");
-};
